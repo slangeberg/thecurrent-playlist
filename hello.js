0e1cd7ec4ff144b005bea93c492f5d0188fb8d1b
Scott Langeberg <slangeberg@gmail.com>
2014-11-10 11:09:53 -0600

    Patching playlist request

--- hello.js
+++ hello.js
@@ -13,9 +13,11 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
 
-      var result = HTTP.call("GET", "http://api.twitter.com/xyz");
+      //HTTP.call("GET", "http://www.thecurrent.org/playlist", function(result){
+      //  console.log("result: ", result);
+      //  $('.output').html('result: ' + result);
+      //})
 
-      $('.output').html('result: ' + result);
       return true;
     }
   });
@@ -25,5 +27,15 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     console.log("Meteor.startup() - isServer");
+
+    try {
+      var result = HTTP.call("GET", "http://www.thecurrent.org/playlist");
+      if (result.statusCode == 200) {
+        console.log("SUCCESS: ");
+        console.log("content: " + result.content.substring(0, 1000) + "...")
+      }
+    } catch(ex) {
+      console.error("Unable to fetch playlist. ex: ", ex);
+    }
   });
-}
+} 
